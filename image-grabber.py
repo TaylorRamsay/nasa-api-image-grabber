e579905db6ffcb0a723f71399b2caafcb83de5ac
tRamsay93 <t_ramsay@csu.fullerton.edu>
2022-09-06 17:04:27 -0700

    Implemented Image requesting api + file saving

--- image-grabber.py
+++ image-grabber.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import shutil
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 from urllib.request import urlretrieve
 from pprint import PrettyPrinter
@@ -7,34 +11,29 @@ pp = PrettyPrinter()
 
 api_key  = '3AvahtwGwoLngcCHiVmHu8ZzukjJLlnk2kh3nUe7'
 
-def fetchAPOD():
-  URL_APOD = "https://api.nasa.gov/planetary/apod"
-  date = '2020-01-22'
-  params = {
-      'api_key':api_key,
-      'date':date,
-      'hd':'True'
-  }
-  #response = requests.get(URL_APOD,params=params).json()
-  #pp.pprint(response)
-  
-#fetchAPOD()
-
-#params = {
- #   'api_key':api_key
-#}
-
-#response = requests.get('https://api.nasa.gov/EPIC/api/natural/images', params).json()
-
-#response = requests.get('https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png', params).json()
-
-#pp.pprint(response)
-
-#photo = requests.get('https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/epic_1b_20190530011359.png', params)
-
 epic = requests.get('https://api.nasa.gov/EPIC/api/natural/images?api_key=3AvahtwGwoLngcCHiVmHu8ZzukjJLlnk2kh3nUe7')
 data = json.loads(epic.text)
 
 for x in data:
   testPhoto = (x['image'])
-  photo = requests.get('https://api.nasa.gov/EPIC/archive/natural/png/' + testPhoto + '?api_key=' + api_key)
+  testPhotoDate = (x['date'])
+  formattedDate = testPhotoDate.replace('-', '/')
+  dateList = formattedDate.split()
+  print(testPhoto + ', ' + dateList[0])
+
+  photo = requests.get('https://api.nasa.gov/EPIC/archive/natural/' + dateList[0] + '/png/' + testPhoto + '.png?api_key=' + api_key, stream = True)
+  
+  file_name = input('Save image as (string):')
+
+  if photo.status_code == 200:
+    with open(file_name, 'wb') as f:
+      shutil.copyfileobj(photo.raw, f)
+    print('Image sucessfully downloaded: ', file_name)
+  else:
+    print('Image could not be retreived')
+  
+  #print(photo)
+  #print(type(photo))
+  #img = mpimg.imread(photo)
+  #imgplot = plt.imshow(img)
+  #plt.show()
